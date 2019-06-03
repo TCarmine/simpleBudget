25f4c340001118e1a8dee5006181435559dc0b77
TCarmine <kappa85t@hotmail.it>
2019-06-04 00:11:33 +0200

    expense-list logic

--- js/app.js
+++ js/app.js
@@ -17,17 +17,61 @@ class UI {
   }
   // submit budget method
   submitBudgetForm(){
-    const valueInput = this.budgetInput.value;
-    if( valueInput === ""|| valueInput < 0){
-      this.budgetFeedback.classList.add("showItem");
-      this.budgetFeedback.innerHTML = `<p>Insert a non zero and positive value</p>`;
-      setTimeout(()=>{
-        this.budgetFeedback.classList.remove("showItem");
-      },1500);
+      const valueInput = this.budgetInput.value;
+      if( valueInput === ""|| valueInput < 0){
+        this.budgetFeedback.classList.add("showItem");
+        this.budgetFeedback.innerHTML = `<p>Insert a non zero and positive value</p>`;
+        // it is need to explicit point "this" that after been called refer to windows
+        // global Object
+        const self = this;
+        setTimeout(()=>{
+          self.budgetFeedback.classList.remove("showItem");
+        },2000);
+
+      }else{
+        this.budgetAmount.textContent = valueInput;
+        this.budgetInput.value = '';
+        this.showBalance();
+      }
+  }
+  //show balance
+  showBalance(){
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent)-expense;
+    this.balanceAmount.textContent = total;
+    if(total < 0){
+      this.balance.classList.remove('showGreen','showBlack');
+      this.balance.classList.add('showred');
+    } else  if(total === 0){
+      this.balance.classList.remove('showred','showGreen');
+      this.balance.classList.add('showBlack');
     }
   }
+  totalExpense(){
+    let total =  400;
+    return total;
+  }
+  // total expense
+/*   submitExpenseForm(){
+        const expenseInput = this.expenseInput.value;
+        if( expenseInput === ""|| expenseInput < 0){
+          this.expenseFeedback.classList.add("showItem");
+          this.expenseFeedback.innerHTML = `<p>Insert a non zero and positive value</p>`;
+          // it is need to explicit point "this" that after been called refer to windows
+          // global Object
+          const self = this;
+          setTimeout(()=>{
+            self.expenseFeedback.classList.remove("showItem");
+          },2000);
+
+        }else{
+          this.expenseFeedback.value = expenseInput;
+          this.expenseInput.value= ''; 
+        }
+  } */
+
   
-}
+}  
 
 
 
